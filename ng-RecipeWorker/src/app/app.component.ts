4ceda36defc2ec69ff645385505dc1f921be468e
cajmrn <cajmrn@gmail.com>
2018-12-24 15:16:32 -0400

    added some minor nagivation tweeks

--- ng-RecipeWorker/src/app/app.component.ts
+++ ng-RecipeWorker/src/app/app.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  loadedFeature: string = 'R';
+
+  onNavigate(e: string){
+    this.loadedFeature = e;
+  }
+}
